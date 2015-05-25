5b76d19d86dc8a5446d3a7ee25444070f67965f6
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-25 15:32:03 -0700

    Create cube2igraph.R

---
+++ cube2igraph.R
@@ -0,0 +1,3 @@
+#Stores a Gaussian cube file as a simplified series of igraph objects over atomic "nodes". The purpose of this will be to perform
+#frontier MO reaction estimations from a very small file. This will be most useful in assigning chemical reaction probability
+#rankings to large sets of molecules.
