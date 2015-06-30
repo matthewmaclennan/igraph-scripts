d628f10eb7cdec85c06e322bcff9d0aaa5b8ea33
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-30 14:08:04 -0700

    Update sdfset2igraph.R

--- sdfset2igraph.R
+++ sdfset2igraph.R
@@ -1,8 +1,8 @@
 #Program for converting SMILES strings to igraph undirected graphs.
-sdfset2igraphU<-function(sdfset){
+sdfset2igraph<-function(sdfset,directed){
 require(igraph)
 require(ChemmineR)
-  sbbmg<-lapply(bondblock(sdfset),function(x) graph.edgelist(matrix(x[,1:2],ncol=2),directed=F))
+  sbbmg<-lapply(bondblock(sdfset),function(x) graph.edgelist(matrix(x[,1:2],ncol=2),directed=directed))
   atomnames<-lapply(atomblock(sdfset),function(x) unlist(strsplit(rownames(x),"_[0-9]+"),recursive=F))
   for(i in 1:length(sdfset)){
     V(sbbmg[[i]])$name<-atomnames[[i]]
