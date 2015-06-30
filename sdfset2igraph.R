5818b037ca25ff80c3036512b610f077532153ef
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-30 14:06:23 -0700

    Update sdfset2igraph.R

--- sdfset2igraph.R
+++ sdfset2igraph.R
@@ -2,7 +2,7 @@
 sdfset2igraphU<-function(sdfset){
 require(igraph)
 require(ChemmineR)
-  sbbmg<-lapply(bondblock(sdfset),function(x) graph(matrix(x[,1:2],ncol=2),directed=F))
+  sbbmg<-lapply(bondblock(sdfset),function(x) graph.edgelist(matrix(x[,1:2],ncol=2),directed=F))
   atomnames<-lapply(atomblock(sdfset),function(x) unlist(strsplit(rownames(x),"_[0-9]+"),recursive=F))
   for(i in 1:length(sdfset)){
     V(sbbmg[[i]])$name<-atomnames[[i]]
