09b15f88364c00dc751f9975a73138e1d88d191e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-30 13:50:19 -0700

    Update smiles2igraph.R

--- smiles2igraph.R
+++ smiles2igraph.R
@@ -1,1 +1,9 @@
 #Program for converting SMILES strings to igraph objects (directed graphs)
+smiles2igraph<-function(SMILES){
+require(igraph)
+require(ChemmineR)
+  sdf.set<-smiles2sdf(SMILES)
+  sbbmg<-graph.edgelist(matrix(bondblock(sdf.set),ncol=2),directed=F)
+  V(sbbmg)$name<-unlist(strsplit(rownames(atomblock(sdf.set)),"_[0-9]+"))
+  
+}
